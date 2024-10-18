13f788e6c7f45bfdd61690b3ffeddf6aa3ad32d5
clydeador123545 <clyde.ador@ciit.edu.ph>
2024-10-19 00:14:42 +0800

    third

---
+++ js/header.js
@@ -0,0 +1,14 @@
+function loadHeader() {
+
+    const header = `
+        <ul>
+            <li class="home"><a href="/index.html">Home</a></li>
+            <li><a href="components/about.html">About us</a></li>
+            <li>Contact us</li>
+        </ul>
+    `;
+    
+    document.getElementById('header').innerHTML = header;
+   
+}
+window.onload = loadHeader;
